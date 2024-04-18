0c937d9d76a287491a1932ce3b08eab9e7f9a711
DEH <156702447+DEH10@users.noreply.github.com>
2024-04-18 15:59:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,40 +4,25 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const apiKey = '7b921481edf0984cd4518d191e97bd356419a5977fe37dc9fef483664ff8554e';
 
 // Enable CORS
 app.use(cors());
 
-// Set X-Content-Type-Options header
-app.use(helmet({
-  contentSecurityPolicy: false,
-  referrerPolicy: { policy: 'no-referrer' }
-}));
-
-// Define a route to handle the news search
-app.get('/search', async (req, res) => {
-    const topic = req.query.topic;
-    const apiUrl = `https://serpapi.com/search.json?q=${encodeURIComponent(topic)}&tbm=nws&api_key=${apiKey}`;
+// Define a route to handle proxy requests to SerpApi
+app.get('/proxy', async (req, res) => {
+    const { q, tbm, api_key } = req.query;
+    const apiUrl = `https://serpapi.com/search.json?q=${encodeURIComponent(q)}&tbm=${encodeURIComponent(tbm)}&api_key=${encodeURIComponent(api_key)}`;
 
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
-            throw new Error('Failed to fetch news');
+            throw new Error('Failed to fetch data from SerpApi');
         }
         const data = await response.json();
-
-        // Set Cache-Control header to prevent using the Expires header
-        res.setHeader('Cache-Control', 'no-cache'); // Example cache-control header
-
-        if (data.news_results && data.news_results.length > 0) {
-            res.json(data.news_results);
-        } else {
-            res.status(404).json({ message: 'No news articles found' });
-        }
+        res.json(data);
     } catch (error) {
-        console.error('Error fetching news:', error.message);
-        res.status(500).json({ error: 'Failed to fetch news' });
+        console.error('Error proxying request to SerpApi:', error);
+        res.status(500).json({ error: 'Failed to proxy request to SerpApi' });
     }
 });
 

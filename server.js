32f54581b4fb734713760e101310a20f088a42ed
DEH <156702447+DEH10@users.noreply.github.com>
2024-04-18 17:07:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const cors = require('no-cors');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;

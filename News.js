66bcd4a6fb57ddcb2bfb3b2e3ee814fa2aac6580
DEH <156702447+DEH10@users.noreply.github.com>
2024-04-20 15:26:14 -0500

    Update News.js

--- News.js
+++ News.js
@@ -78,7 +78,7 @@ function displayNewsOnPage(news) {
     newsContainer.innerHTML = '';
 
     // Iterate over each news article and create HTML elements to display them
-    responseData.forEach(newsItem => {
+    responseData.results.forEach(newsItem => {
         // Create a container for each news article
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('news-item');

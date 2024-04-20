b9003715f300cdf4ffd783ce7e6d8dcb5306027e
DEH <156702447+DEH10@users.noreply.github.com>
2024-04-20 09:41:11 -0500

    Update News.js

--- News.js
+++ News.js
@@ -18,8 +18,10 @@ async function searchTopic(topic, apiKey) {
     const requestOptions = {
         method: 'GET',
         headers: {
-            'X-Api-Key': newsApiKey,
+            'Accept': 'application/json',
+            'Cache-Control': 'no-cache',
             'Authorization': newsApiKey // Either of these headers can be used for authentication
+            
         }
     };
 
